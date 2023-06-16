ce6adc6fd92cd1928ff92dba745f113c5ff059d4
cbepxbeo <cbepxbeo@mail.ru>
2023-06-16 18:55:05 +0300

    add FluentExtensionsError

---
+++ Sources/FluentExtensionsError/FluentExtensionsError.swift
@@ -0,0 +1,19 @@
+/*
+
+Project: FluentExtensions
+File: FluentExtensionsError.swift
+Created by: Egor Boyko
+Date: 16.01.2022
+Last Fix: 16.06.2023
+Version: 0.0.1
+
+Status: #Complete | #Decorated
+
+*/
+
+///Namespace for errors within a package
+enum FluentExtensionsError {}
+
+
+
+
