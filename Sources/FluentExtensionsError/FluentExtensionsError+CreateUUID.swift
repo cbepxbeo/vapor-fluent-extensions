15770e45b3c20fc9173db32c4ce62159820a6113
cbepxbeo <cbepxbeo@mail.ru>
2023-06-17 15:49:32 +0300

    Update FluentExtensionsError+CreateUUID.swift
    add case dependencyMySQLKitNotImported

--- Sources/FluentExtensionsError/FluentExtensionsError+CreateUUID.swift
+++ Sources/FluentExtensionsError/FluentExtensionsError+CreateUUID.swift
@@ -4,7 +4,7 @@ Project: FluentExtensions
 File: FluentExtensionsError+CreateUUID.swift
 Created by: Egor Boyko
 Date: 16.01.2022
-Last Fix: 16.06.2023
+Last Fix: 17.06.2023
 Version: 0.0.1
 
 Status: #Complete | #Decorated
@@ -19,5 +19,7 @@ extension FluentExtensionsError {
         case dataBaseFieldNotFound
         ///Error signaling changes in dependent libraries since the method uses a hack to validate.
         case builderDoesNotContainDatabase
+        ///An error that signals a missing dependency for the method to work.
+        case dependencyMySQLKitNotImported(message: String)
     }
 }
