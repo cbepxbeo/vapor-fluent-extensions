99d2df379148a59195d0bd114cc6ff67a76646e5
cbepxbeo <cbepxbeo@mail.ru>
2023-06-17 15:49:03 +0300

    Update Package.swift
    Downgraded compatible version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "vapor-fluent-extensions",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v12)
     ],
     products: [
         .library(
